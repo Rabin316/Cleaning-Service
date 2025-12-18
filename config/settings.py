4e3f2016141f0829a4fa126a068fd2615531b3d4
Rabin316 <rabindangol12345@gmail.com>
2025-12-18 12:54:10 +0545

    host

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
