4ca7d72366448f97213a95aaca4857e399df7c1d
Rabin316 <rabindangol12345@gmail.com>
2025-12-18 11:46:20 +0545

    fix

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
